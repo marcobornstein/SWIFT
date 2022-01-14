9a41edec44fb71f02809055a00d0575b4a85d6bd
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-01-14 11:51:43 -0500

    Add AsyncComm Code Prototype

---
+++ AsynchronousFederated/AsyncCommunicator.py
@@ -0,0 +1,93 @@
+import numpy as np
+import time
+from mpi4py import MPI
+import torch
+from comm_helpers import flatten_tensors, unflatten_tensors
+
+
+class AsyncDecentralized:
+
+    def __init__(self, rank, size, topology, sgd_updates):
+        """ Initialize the Asynchronous Decentralized Communicator """
+
+        # Graph initialization
+        self.topology = topology
+        self.neighbor_list = self.topology.neighbors(rank)
+        self.neighbor_weights = topology.neighbor_weights
+        self.degree = len(self.neighbor_list)
+
+        # Initialize MPI variables
+        self.comm = MPI.COMM_WORLD
+        self.rank = rank
+        self.size = size
+        self.requests = [MPI.REQUEST_NULL for _ in range(self.degree)]
+
+        self.sgd_updates = sgd_updates
+        self.iter = 0
+
+    def prepare_send_buffer(self, model):
+        # stack all model parameters into one tensor list
+        self.tensor_list = list()
+        for param in model.parameters():
+            self.tensor_list.append(param)
+        # faltten tensors
+        self.send_buffer = flatten_tensors(self.tensor_list).cpu()
+
+
+    def reset_model(self):
+        # Reset local models to be the averaged model
+        for f, t in zip(unflatten_tensors(
+                        self.avg_model.cuda(), self.tensor_list),
+                        self.tensor_list):
+            with torch.no_grad():
+                t.set_(f)
+
+    def averaging(self, model):
+
+        # necessary preprocess
+        self.prepare_send_buffer(model)
+        self.avg_model = torch.zeros_like(self.send_buffer)
+
+        tic = time.time()
+
+        # compute weighted average: (1-d*alpha)x_i + alpha * sum_j x_j
+        for node in self.neighbor_list:
+            worker_model = self.comm.recv(source=node, tag=node)
+            self.avg_model.add_(worker_model, alpha=self.neighbor_weights[node])
+
+        # compute self weight according to degree
+        selfweight = 1 - np.sum(self.neighbor_weights)
+
+        # compute weighted average: (1-d*alpha)x_i + alpha * sum_j x_j
+        self.avg_model.add_(self.send_buffer, alpha=selfweight)
+
+        toc = time.time()
+
+        # update local models
+        self.reset_model()
+
+        return toc - tic
+
+    def broadcast(self, model):
+        # necessary preprocess
+        self.prepare_send_buffer(model)
+
+        tic = time.time()
+
+        for idx, node in enumerate(self.neighbor_list):
+            self.requests[idx] = self.comm.isend(self.send_buffer, dest=node, tag=self.rank)
+
+        toc = time.time()
+
+        return toc - tic
+
+    def communicate(self, model):
+
+        self.iter += 1
+
+        if self.iter % self.sgd_updates == 0:
+            comm_time = self.averaging(model)
+        else:
+            comm_time = self.broadcast(model)
+
+        return comm_time
