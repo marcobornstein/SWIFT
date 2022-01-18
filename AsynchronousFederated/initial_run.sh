74b58c75c7c7f40025fc17fe3801d312c9b29dac
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-01-18 11:33:25 -0500

    Edit/Add Train MPI

--- AsynchronousFederated/initial_run.sh
+++ AsynchronousFederated/initial_run.sh
@@ -2,16 +2,16 @@
 
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
-#SBATCH --job-name=matcha     # sets the job name if not set from environment
-#SBATCH --time=00:30:00                                         # how long you think your job will take to complete; format=hh:mm:ss
-#SBATCH --account=scavenger                                     # set QOS, this will determine what resources can be requested
-#SBATCH --qos=scavenger                                         # set QOS, this will determine what resources can be requested
+#SBATCH --job-name=AsyncDecentralized     # sets the job name if not set from environment
+#SBATCH --time=00:30:00                   # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --account=scavenger               # set QOS, this will determine what resources can be requested
+#SBATCH --qos=scavenger                   # set QOS, this will determine what resources can be requested
 #SBATCH --partition=scavenger
 #SBATCH --gres=gpu:2
 #SBATCH --ntasks=8
-#SBATCH --mem 64gb                                              # memory required by job; if unit is not specified MB will be assumed
+#SBATCH --mem 64gb                        # memory required by job; if unit is not specified MB will be assumed
 #SBATCH --nice=0
-#SBATCH --mail-type=END                 # Valid type values are NONE, BEGIN, END, FAIL, REQUEUE,
+#SBATCH --mail-type=END                   # Valid type values are NONE, BEGIN, END, FAIL, REQUEUE,
 #SBATCH --error Logs/%x_%A_%a.log
 
 #module load python
