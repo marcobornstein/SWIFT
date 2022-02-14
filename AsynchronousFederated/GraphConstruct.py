367862ee1bc4c502fccf382153df27acacb63770
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-02-14 12:29:08 -0500

    build in graph construction

--- AsynchronousFederated/GraphConstruct.py
+++ AsynchronousFederated/GraphConstruct.py
@@ -11,11 +11,28 @@ class GraphConstruct:
         self.size = size  # totoal number of workers
         self.comm = MPI.COMM_WORLD
 
-        self.graph = graph
+        self.graph = self.selectGraph(graph)
 
         # Determine each node's neighbors and the weights for each node in the Graph
         self.neighbor_list = self.getNeighbors(rank)
         self.neighbor_weights = self.getWeights()
+
+    def selectGraph(self, graph):
+
+        if isinstance(graph, list):
+            return graph
+        else:
+            g = []
+            if graph == 'fully-connected':
+                for i in range(self.size):
+                    for j in range(i+1, self.size):
+                        g.append((i, j))
+            elif graph == 'ring':
+                for i in range(self.size):
+                    if i != self.size - 1:
+                        g.append((i, i+1))
+                    else:
+                        g.append((i, 0))
 
     def getWeights(self, weight_type=None):
         
--- AsynchronousFederated/Train.py
+++ AsynchronousFederated/Train.py
@@ -37,9 +37,9 @@ def run(rank, size):
     # load base network topology
     # Graph = [(0, 1)]  # Simple Link
     # Graph = [(0, 1), (0, 2), (1, 2)]  # Simple Triangle
-    Graph = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]  # Inter-connected Square
+    # Graph = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]  # Inter-connected Square
 
-    GP = GraphConstruct(Graph, rank, size)
+    GP = GraphConstruct('fully-connected', rank, size)
     sgd_steps = 3
     communicator = AsyncDecentralized(rank, size, GP, sgd_steps, args.max_sgd)
 
