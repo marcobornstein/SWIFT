11b766d9d7bd809ac1e118eb885675a40dabfdca
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-02-14 12:29:35 -0500

    build in graph construction

--- AsynchronousFederated/run-personalized.sh
+++ AsynchronousFederated/run-personalized.sh
@@ -16,4 +16,4 @@
 module load openmpi
 module load cuda/11.2.2
 
-mpirun -np 4 python Train.py  --personalize 1 --max_sgd 10 --resSize 50 --bs 64 --epoch 150 --name square-pers --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 4 python Train.py  --personalize 1 --max_sgd 10 --resSize 50 --bs 64 --epoch 15 --name square-pers --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
