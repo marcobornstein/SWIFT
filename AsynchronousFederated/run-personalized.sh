57c9f7dc7f7f0831e2f24b4f143d13550116dc0f
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-02-14 23:19:43 -0500

    test out addition of ring of cliques

--- AsynchronousFederated/run-personalized.sh
+++ AsynchronousFederated/run-personalized.sh
@@ -16,4 +16,4 @@
 module load openmpi
 module load cuda/11.2.2
 
-mpirun -np 8 python Train.py  --graph clique-ring --num_clusters 2 --personalize 1 --max_sgd 10 --resSize 50 --bs 64 --epoch 15 --name square-pers --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 9 python Train.py  --graph clique-ring --num_clusters 3 --personalize 1 --max_sgd 10 --resSize 50 --bs 64 --epoch 15 --name square-pers --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
