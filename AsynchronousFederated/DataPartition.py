d593bdac39313f4bd2a08a2129a9fd914fb089c9
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-02-15 13:25:44 -0500

    test non-iid

--- AsynchronousFederated/DataPartition.py
+++ AsynchronousFederated/DataPartition.py
@@ -22,7 +22,7 @@ class Partition(object):
         return self.data[data_idx]
 
 class DataPartitioner(object):
-    """ Partitions a dataset into different chuncks. """
+    """ Partitions a dataset into different chunks. """
     def __init__(self, data, sizes=[0.7, 0.2, 0.1], seed=1234, isNonIID=False):
         self.data = data
         self.partitions = []
@@ -32,65 +32,85 @@ class DataPartitioner(object):
         indexes = [x for x in range(0, data_len)]
         rng.shuffle(indexes)
 
-
-        for frac in sizes:
-            part_len = int(frac * data_len)
-            self.partitions.append(indexes[0:part_len])
-            indexes = indexes[part_len:]
-
         if isNonIID:
-            self.partitions = self.getNonIIDdata(self, data, sizes, seed)
+            self.partitions = self.getNonIIDdata(data, sizes, seed)
+        else:
+            for frac in sizes:
+                part_len = int(frac * data_len)
+                self.partitions.append(indexes[0:part_len])
+                indexes = indexes[part_len:]
 
     def use(self, partition):
         return Partition(self.data, self.partitions[partition])
 
     def getNonIIDdata(self, data, sizes, seed):
-        labelList = data.train_labels
+
         rng = Random()
         rng.seed(seed)
-        a = [(label, idx) for idx, label in enumerate(labelList)]
-        # Same Part
+
+        # Determine labels & create a dictionary storing all data point indices with their corresponding label
+        labelList = data.train_labels
         labelIdxDict = dict()
-        for label, idx in a:
-            labelIdxDict.setdefault(label ,[])
+        for idx, label in enumerate(labelList):
+            labelIdxDict.setdefault(label, [])
             labelIdxDict[label].append(idx)
+
+        # Determine number of labels and create a list of these labels
         labelNum = len(labelIdxDict)
         labelNameList = [key for key in labelIdxDict]
-        labelIdxPointer = [0] * labelNum
-        partitions = [list() for i  in range(len(sizes))]
-        eachPartitionLen= int(len(labelList ) /len(sizes))
-        majorLabelNumPerPartition = ceil(labelNum /len(partitions))
-        basicLabelRatio = 0.4
 
+        # Create list of indices which point to most recent corresponding label in the data
+        labelIdxPointer = [0] * labelNum
+
+        # Create partition of the data for each worker
+        partitions = [list() for _ in range(len(sizes))]
+        eachPartitionLen = int(len(labelList)/len(sizes))
+        # Determine the number of labels per worker (num partitions)
+        majorLabelNumPerPartition = ceil(labelNum/len(partitions))
+
+        basicLabelRatio = 0.4
         interval = 1
         labelPointer = 0
 
         # basic part
+        # iterate through each of the partitions
         for partPointer in range(len(partitions)):
+            # create a list of labels that will be predominant for a worker
             requiredLabelList = list()
             for _ in range(majorLabelNumPerPartition):
+                # add the predominant labels to the list
                 requiredLabelList.append(labelPointer)
                 labelPointer += interval
                 if labelPointer > labelNum - 1:
                     labelPointer = interval
                     interval += 1
+            # add in these predominant labels to the partition of the worker
             for labelIdx in requiredLabelList:
                 start = labelIdxPointer[labelIdx]
-                idxIncrement = int(basicLabelRatio *len(labelIdxDict[labelNameList[labelIdx]]))
-                partitions[partPointer].extend(labelIdxDict[labelNameList[labelIdx]][start:start+ idxIncrement])
+                idxIncrement = int(basicLabelRatio*len(labelIdxDict[labelNameList[labelIdx]]))
+                partitions[partPointer].extend(labelIdxDict[labelNameList[labelIdx]][start:start+idxIncrement])
                 labelIdxPointer[labelIdx] += idxIncrement
 
         # random part
+        # construct a list of the remianing data points that haven't been added to a partition
         remainLabels = list()
         for labelIdx in range(labelNum):
             remainLabels.extend(labelIdxDict[labelNameList[labelIdx]][labelIdxPointer[labelIdx]:])
+
+        # randomly shuffle the labels up so they are not in order by their label
         rng.shuffle(remainLabels)
+
+        # iterate over the workers to add in random labels to their partition
         for partPointer in range(len(partitions)):
+            # find the gap needed to be filled to meet the expected partition length (needed - what is there already)
             idxIncrement = eachPartitionLen - len(partitions[partPointer])
+            # fill the partition to the desired length
             partitions[partPointer].extend(remainLabels[:idxIncrement])
+            # randomly shuffle the partition
             rng.shuffle(partitions[partPointer])
             remainLabels = remainLabels[idxIncrement:]
         return partitions
+
 
 def partition_dataset(rank, size, args):
 
@@ -119,7 +139,7 @@ def partition_dataset(rank, size, args):
 
         partition_sizes = [1.0 / size for _ in range(size)]
 
-        partition = DataPartitioner(trainset, partition_sizes, isNonIID=False)
+        partition = DataPartitioner(trainset, partition_sizes, isNonIID=True)
         partition = partition.use(rank)
 
         train_loader = torch.utils.data.DataLoader(partition,
@@ -143,9 +163,7 @@ def partition_dataset(rank, size, args):
 
         test_loader = torch.utils.data.DataLoader(testset,
                                                   batch_size=64,
-                                                  shuffle=False,
-                                                  num_workers=size)
+                                                  shuffle=False)
         MPI.COMM_WORLD.Barrier()
-
 
     return train_loader, test_loader
