4c6c13d783993944f8eff8fba5b840bced475f94
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-02-22 13:04:30 -0500

    try out test-acc weight setting

--- AsynchronousFederated/test-comms.sh
+++ AsynchronousFederated/test-comms.sh
@@ -16,7 +16,7 @@
 module load openmpi
 module load cuda/11.1.1
 
-mpirun -np 4 python Train.py  --graph fully-connected --name dsgd-test --comm_style d-sgd --resSize 50 --bs 64 --epoch 15 --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 4 python Train.py  --graph fully-connected --name pdsgd-test --comm_style pd-sgd --i1 2 --resSize 50 --bs 64 --epoch 15  --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 4 python Train.py  --graph fully-connected --name ldsgd-test  --comm_style ld-sgd --i1 2 --i2 2 --resSize 50 --bs 64 --epoch 15  --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 4 python Train.py  --graph fully-connected --name padfed-test --sgd_steps 1 --num_clusters 1 --personalize 1 --max_sgd 10 --resSize 50 --bs 64 --epoch 15  --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 5 python Train.py  --graph fully-connected --name dsgd-test --comm_style d-sgd --resSize 50 --bs 64 --epoch 25 --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 5 python Train.py  --graph fully-connected --name pdsgd-test --comm_style pd-sgd --i1 1 --resSize 50 --bs 64 --epoch 25  --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 5 python Train.py  --graph fully-connected --name ldsgd-test  --comm_style ld-sgd --i1 1 --i2 2 --resSize 50 --bs 64 --epoch 25  --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 5 python Train.py  --graph fully-connected --name padfed-test --sgd_steps 2 --num_clusters 1 --personalize 1 --max_sgd 10 --resSize 50 --bs 64 --epoch 25  --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
