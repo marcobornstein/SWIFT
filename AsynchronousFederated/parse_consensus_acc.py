b3b76931acce96e663e220ef8c6cdc840b6d0115
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-02-26 14:22:42 -0500

    parse results

--- AsynchronousFederated/parse_consensus_acc.py
+++ AsynchronousFederated/parse_consensus_acc.py
@@ -41,6 +41,36 @@ def unpack_data2(directory_path, epoch, num_workers, datatype):
     return data
 
 
+def unpack_data3(directory_path, epoch, num_workers, datatype, communicator):
+
+    directory = os.path.join(directory_path)
+    data = np.zeros((6, num_workers))
+    count = 0
+
+    for root, dirs, files in os.walk(directory):
+
+        for dir in dirs:
+            if dir.startswith(communicator):
+                new_directory_path = directory_path + '/' + dir
+                new_directory = os.path.join(new_directory_path)
+                temp_data = np.zeros((epoch, num_workers))
+                for root, dirs, files in os.walk(new_directory):
+                    for file in files:
+                        if file.endswith(datatype):
+                            j = int(file[1])
+                            f = open(new_directory_path + '/' + file, 'r')
+                            i = 0
+                            for line in f:
+                                temp_data[i, j] = line
+                                i += 1
+                # cum_temp_data = np.cumsum(temp_data)
+                # data[count, :] = cum_temp_data[-1, :]
+                data[count, :] = np.sum(temp_data, axis=0)
+                count += 1
+        data[-1, :] = np.average(data[:-1, :], axis=0)
+    return data
+
+
 if __name__ == "__main__":
 
     args = sys.argv
@@ -52,22 +82,30 @@ if __name__ == "__main__":
     epoch = int(args[2])
     num_workers = int(args[3])
 
+    time_data = unpack_data3(path, epoch, num_workers, 'total-time.log', 'run-dsgd')
+    print(time_data)
+
     # acc_data = unpack_data(path, epoch, 1, 'consensus-average')
     # print(acc_data[0])
     # print(acc_data[9::10])
 
-    time_data = unpack_data2(path, epoch, num_workers, 'epoch-time.log')
+    '''
+    time_data = unpack_data2(path, epoch, num_workers, 'total-time.log')
     cum_time_data = np.cumsum(time_data, axis=0)
-    # print(cum_time_data[-1, :])
+    print('Total Time Until Completion for each Worker:')
+    print(cum_time_data[-1, :])
+    print('Average Time per Epoch for each Worker:')
+    print(cum_time_data[-1, :]/epoch)
+
+    time_data2 = unpack_data2(path, epoch, num_workers, 'epoch-time.log')
     avg_time_data = np.average(time_data, axis=0)
     min_time_data = np.min(time_data, axis=0)
-    max_time_data = np.max(time_data, axis=0)
-
     print('Average Epoch Time For Each Worker:')
     print(avg_time_data)
     print('Minimum Epoch Time For Each Worker:')
     print(min_time_data)
-    print('Maximum Epoch Time For Each Worker:')
-    print(max_time_data)
-
-
+    # max_time_data = np.max(time_data, axis=0)
+    # print('Maximum Epoch Time For Each Worker:')
+    # print(max_time_data)
+    print('==================================================================')
+    '''
