343d9a6973fc555c11fb33645abb1d0e23346a83
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-02-26 03:09:29 -0500

    Run Experiment - 1.00 non-iid (5 workers for CIFAR10)

--- AsynchronousFederated/async-regular.sh
+++ AsynchronousFederated/async-regular.sh
@@ -4,9 +4,9 @@
 
 #SBATCH --job-name=AdFed     # sets the job name if not set from environment
 #SBATCH --time=04:45:00     # how long you think your job will take to complete; format=hh:mm:ss
-#SBATCH --account=furongh    # set QOS, this will determine what resources can be requested
-#SBATCH --qos=medium   # set QOS, this will determine what resources can be requested
-#SBATCH --partition=dpart
+#SBATCH --account=scavenger    # set QOS, this will determine what resources can be requested
+#SBATCH --qos=scavenger  # set QOS, this will determine what resources can be requested
+#SBATCH --partition=scavenger
 #SBATCH --gres=gpu:2
 #SBATCH --ntasks=8
 #SBATCH --mem 64gb         # memory required by job; if unit is not specified MB will be assumed
@@ -16,8 +16,8 @@
 module load openmpi
 module load cuda/11.1.1
 
-mpirun -np 6 python Train.py --name async-0.75-noniid-5sgdmax-2sgdstep-fc-5-test1 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 0.75 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 6 python Train.py --name async-0.75-noniid-5sgdmax-2sgdstep-fc-5-test2 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 0.75 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 6 python Train.py --name async-0.75-noniid-5sgdmax-2sgdstep-fc-5-test3 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 0.75 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 6 python Train.py --name async-0.75-noniid-5sgdmax-2sgdstep-fc-5-test4 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 0.75 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 6 python Train.py --name async-0.75-noniid-5sgdmax-2sgdstep-fc-5-test5 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 0.75 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name async-1-noniid-5sgdmax-2sgdstep-fc-5-test1 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name async-1-noniid-5sgdmax-2sgdstep-fc-5-test2 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name async-1-noniid-5sgdmax-2sgdstep-fc-5-test3 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name async-1-noniid-5sgdmax-2sgdstep-fc-5-test4 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name async-1-noniid-5sgdmax-2sgdstep-fc-5-test5 --graph fully-connected --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
