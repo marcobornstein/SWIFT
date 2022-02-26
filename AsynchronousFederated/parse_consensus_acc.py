3e30aa57d519d26c45a6fd8b396a7585d000ae88
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-02-26 17:40:33 -0500

    parse results and fix save folder bug causing issues with dsgd

--- AsynchronousFederated/parse_consensus_acc.py
+++ AsynchronousFederated/parse_consensus_acc.py
@@ -49,7 +49,6 @@ def unpack_data3(directory_path, epoch, num_workers, datatype, communicator):
     for root, dirs, files in os.walk(directory):
 
         for dir in dirs:
-            print(dir)
             if dir.startswith(communicator):
                 count = int(dir[-11]) - 1
                 new_directory_path = directory_path + '/' + dir
