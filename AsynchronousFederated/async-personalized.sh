6c7e160e87b92eeaf8e76fb7a208cf620b5e54b3
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-03-07 12:55:54 -0500

    Run Experiments for Ring Graph (5 Workers)

--- AsynchronousFederated/async-personalized.sh
+++ AsynchronousFederated/async-personalized.sh
@@ -16,8 +16,8 @@
 module load openmpi
 module load cuda/11.1.1
 
-mpirun -np 6 python Train.py --name padfed-noniid-test1-5W --graph fully-connected --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 6 python Train.py --name padfed-noniid-test2-5W --graph fully-connected --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 6 python Train.py --name padfed-noniid-test3-5W --graph fully-connected --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 6 python Train.py --name padfed-noniid-test4-5W --graph fully-connected --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 6 python Train.py --name padfed-noniid-test5-5W --graph fully-connected --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name padfed-noniid-test1-5W --graph ring --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name padfed-noniid-test2-5W --graph ring --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name padfed-noniid-test3-5W --graph ring --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name padfed-noniid-test4-5W --graph ring --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 6 python Train.py --name padfed-noniid-test5-5W --graph ring --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 200 --description PadFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
