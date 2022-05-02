b765f4d1f255595cdb9ac9e3dfa48dd7cc167321
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-05-02 12:58:02 -0400

    Switch gradient step to after averaging

--- AsynchronousFederated/test-comms.sh
+++ AsynchronousFederated/test-comms.sh
@@ -3,9 +3,9 @@
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
 #SBATCH --job-name=AsyncDecentralized     # sets the job name if not set from environment
-#SBATCH --time=01:35:00     # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --time=00:35:00     # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --account=furongh    # set QOS, this will determine what resources can be requested
-#SBATCH --qos=high   # set QOS, this will determine what resources can be requested
+#SBATCH --qos=medium  # set QOS, this will determine what resources can be requested
 #SBATCH --partition=dpart
 #SBATCH --gres=gpu:rtx2080ti:3
 #SBATCH --ntasks=12
@@ -19,4 +19,4 @@ module load cuda/11.1.1
 # mpirun -np 11 python Train.py  --graph clique-ring --num_clusters 3 --name dsgd-test --comm_style d-sgd --resSize 50 --bs 64 --epoch 50 --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
 # mpirun -np 11 python Train.py  --graph clique-ring --num_clusters 3 --name pdsgd-test --comm_style pd-sgd --i1 1 --resSize 50 --bs 64 --epoch 25  --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
 # mpirun -np 5 python Train.py  --graph fully-connected --name ldsgd-test  --comm_style ld-sgd --i1 1 --i2 2 --resSize 50 --bs 64 --epoch 25  --description asyncDecentralized --randomSeed 9001 --datasetRoot ./data --outputFolder Output
-mpirun -np 11 python Train.py --name padfed-test --graph clique-ring --num_clusters 3 --sgd_steps 2 --personalize 1 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 100 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
+mpirun -np 11 python Train.py --name adfed-test --graph clique-ring --num_clusters 3 --sgd_steps 2 --personalize 0 --max_sgd 5 --degree_noniid 1 --noniid 1 --resSize 50 --bs 64 --epoch 10 --description AdFed --randomSeed 9001 --datasetRoot ./data --outputFolder Output
