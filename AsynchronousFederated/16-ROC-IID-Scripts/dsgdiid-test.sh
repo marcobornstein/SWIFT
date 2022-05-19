ba96dddd4a6474e0a9067f2da963dfc19754175f
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-05-18 23:18:52 -0400

    Fix scripts

--- AsynchronousFederated/16-ROC-IID-Scripts/dsgdiid-test.sh
+++ AsynchronousFederated/16-ROC-IID-Scripts/dsgdiid-test.sh
@@ -3,7 +3,7 @@
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
 #SBATCH --job-name=DSGD     # sets the job name if not set from environment
-#SBATCH --time=03:30:00     # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --time=04:30:00     # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --account=furongh    # set QOS, this will determine what resources can be requested
 #SBATCH --qos=high    # set QOS, this will determine what resources can be requested
 #SBATCH --gres=gpu:4
@@ -15,8 +15,8 @@
 module load openmpi
 module load cuda/11.1.1
 
-mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test1-16W --comm_style d-sgd --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 2332 --datasetRoot ./data --outputFolder Output
-mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test2-16W --comm_style d-sgd --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 2442 --datasetRoot ./data --outputFolder Output
-mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test3-16W --comm_style d-sgd --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 2992 --datasetRoot ./data --outputFolder Output
-mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test4-16W --comm_style d-sgd --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 4844 --datasetRoot ./data --outputFolder Output
-mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test5-16W --comm_style d-sgd --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 2900 --datasetRoot ./data --outputFolder Output
+mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test1-16W-4c --comm_style d-sgd --lr 0.1 --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 2332 --datasetRoot ./data --outputFolder Output
+mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test2-16W-4c --comm_style d-sgd --lr 0.1 --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 2442 --datasetRoot ./data --outputFolder Output
+mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test3-16W-4c --comm_style d-sgd --lr 0.1 --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 2992 --datasetRoot ./data --outputFolder Output
+mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test4-16W-4c --comm_style d-sgd --lr 0.1 --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 4844 --datasetRoot ./data --outputFolder Output
+mpirun -np 16 python Train.py  --graph clique-ring --num_clusters 4 --name dsgd-iid-test5-16W-4c --comm_style d-sgd --lr 0.1 --momentum 0.9 --degree_noniid 0 --noniid 0 --resSize 50 --bs 64 --epoch 200 --description DSGD-paper --randomSeed 2900 --datasetRoot ./data --outputFolder Output
--- AsynchronousFederated/16-ROC-IID-Scripts/ldsgdiid-test.sh
+++ AsynchronousFederated/16-ROC-IID-Scripts/ldsgdiid-test.sh
@@ -3,7 +3,7 @@
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
 #SBATCH --job-name=LDSGD     # sets the job name if not set from environment
-#SBATCH --time=02:00:00     # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --time=04:50:00     # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --account=furongh    # set QOS, this will determine what resources can be requested
 #SBATCH --qos=high    # set QOS, this will determine what resources can be requested
 #SBATCH --gres=gpu:4
