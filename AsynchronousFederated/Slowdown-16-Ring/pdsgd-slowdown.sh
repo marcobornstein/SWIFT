0de2ef8b4f359f3ffdf215cd06abaa768bd9123d
Marcob1996 <32527769+Marcob1996@users.noreply.github.com>
2022-05-24 09:10:42 -0400

    Add batch scripts for slowdown

--- AsynchronousFederated/Slowdown-16-Ring/pdsgd-slowdown.sh
+++ AsynchronousFederated/Slowdown-16-Ring/pdsgd-slowdown.sh
@@ -3,7 +3,7 @@
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
 #SBATCH --job-name=PDSGD-slow     # sets the job name if not set from environment
-#SBATCH --time=09:30:00     # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --time=19:30:00     # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --account=scavenger    # set QOS, this will determine what resources can be requested
 #SBATCH --qos=scavenger  # set QOS, this will determine what resources can be requested
 #SBATCH --partition=scavenger
--- AsynchronousFederated/Slowdown-16-Ring/swift-slow.sh
+++ AsynchronousFederated/Slowdown-16-Ring/swift-slow.sh
@@ -3,7 +3,7 @@
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
 #SBATCH --job-name=SWIFT     # sets the job name if not set from environment
-#SBATCH --time=9:00:00     # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --time=19:00:00     # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --account=scavenger    # set QOS, this will determine what resources can be requested
 #SBATCH --qos=scavenger  # set QOS, this will determine what resources can be requested
 #SBATCH --partition=scavenger
